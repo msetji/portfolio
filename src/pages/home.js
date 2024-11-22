6141a4cc37e3bb31d1f655752ca38bcc2f6ff408
Michael Setji <michaelpsetji@gmail.com>
2024-11-22 17:08:08 -0500

    title transition

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './Home.css';
 
 const Home = () => {
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('show');
+        } else {
+          entry.target.classList.remove('show'); // Reapply animation when it comes back into view
+        }
+      });
+    });
+
+    const hiddenElements = document.querySelectorAll('.hidden');
+    hiddenElements.forEach((el) => observer.observe(el));
+
+    return () => observer.disconnect();
+  }, []);
+
   return (
     <div className="home-container">
-      <h1 className="home-title">Hello,</h1>
-      <h1 className="home-title">I'm <span className="highlight-text">Michael Setji</span></h1>
-      <p className="home-subtitle">I am an ECE/CS major at <span className="highlight-text">Duke University</span></p>
+      <h1 className="home-title hidden">Hello,</h1>
+      <h1 className="home-title hidden">I'm <span className="highlight-text">Michael Setji</span></h1>
+      <p className="home-subtitle hidden">I am an ECE/CS major at <span className="highlight-text">Duke University</span></p>
     </div>
   );
 };
